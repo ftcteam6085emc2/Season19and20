3b29cddc9facb5c11dea030bb6939c9c9f974787
Xander-6085 <53024030+Xander-6085@users.noreply.github.com>
2019-10-02 20:22:22 -0400

    Update MotorTest.java
    Removed imu due to intializing issues, added servo positions

--- code/MotorTest.java
+++ code/MotorTest.java
@@ -5,16 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
 
 @TeleOp
 
 public class MotorTest extends LinearOpMode {
-    private Gyroscope imu;
     private DcMotor hexMotor;
     private DigitalChannel digitalTouch;
     private DistanceSensor sensorColorRange;
@@ -23,7 +18,6 @@ public class MotorTest extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        imu = hardwareMap.get(Gyroscope.class, "imu");
         DcMotor hexMotor = hardwareMap.get(DcMotor.class, "hexMotor");
         //digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
         //sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
@@ -35,28 +29,36 @@ public class MotorTest extends LinearOpMode {
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
-
-        AngularVelocity degrees;
         double tgtPower = 0;
         while (opModeIsActive()) {
-            degrees = imu.getAngularVelocity(AngleUnit.DEGREES);
             tgtPower = -this.gamepad1.left_stick_y;
             hexMotor.setPower(tgtPower);
             // check to see if we need to move the servo.
             if(gamepad1.y) {
                 // move to 0 degrees.
-                hexMotor.setPower(-0.5);
+                hexMotor.setPower(-1);
             }// else if (gamepad1.x || gamepad1.b) {
+            
                 // move to 90 degrees.
              //   hexMotor.setPower(0.1);
              else if (gamepad1.a) {
-                // move to 180 degrees.
-                hexMotor.setPower(0.5);
+                hexMotor.setPower(1);
+            }
+             if(gamepad1.left_bumper){
+                 servoTest.setPosition(1);
+             }
+            if(gamepad1.right_bumper){
+                servoTest.setPosition(-1);
+            }
+            if(gamepad1.dpad_right){
+                servoTest.setPosition(0.5);
+            }
+            if(gamepad1.dpad_left){
+                servoTest.setPosition(-0.5);
             }
             telemetry.addData("Target Power", tgtPower);
             telemetry.addData("Motor Power", hexMotor.getPower());
             telemetry.addData("Status", "Running");
-            telemetry.addData("imu", Float.toString(degrees.xRotationRate));
             telemetry.update();
 
         }
