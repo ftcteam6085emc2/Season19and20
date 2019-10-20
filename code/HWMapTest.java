754e5bd088dd726a063ccfcabc2c188eb1d26dd2
Xander-6085 <53024030+Xander-6085@users.noreply.github.com>
2019-10-20 15:04:27 -0400

    Put in season 18-19 folder or change first line

---
+++ code/HWMapTest.java
@@ -0,0 +1,79 @@
+package org.firstinspires.ftc.teamcode.Season18and19;
+
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IrSeekerSensor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+//This is NOT an OpMode
+
+public class HWMapTest
+{
+    /* Public OpMode members. */
+    public DcMotor  FrontLeft   = null;
+    public DcMotor  FrontRight  = null;
+    public DcMotor  RearLeft  = null;
+    public DcMotor  RearRight  = null;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public HWMapTest(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        FrontLeft   = hwMap.dcMotor.get("FrontLeft");
+        //rightMotor.setDirection(DcMotor.Direction.FORWARD);
+        FrontRight  = hwMap.dcMotor.get("FrontRight");
+        RearLeft = hwMap.dcMotor.get("RearLeft");
+        RearRight = hwMap.dcMotor.get("RearRight");
+        //RearRight.setDirection(DcMotor.Direction.FORWARD);
+
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        RearLeft.setPower(0);
+        RearRight.setPower(0);
+
+        //Set position of servos
+
+    }
+
+    /***
+     *
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs  Length of wait cycle in mSec.
+     */
+    public void waitForTick(long periodMs) {
+
+        long  remaining = periodMs - (long)period.milliseconds();
+
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0) {
+            try {
+                Thread.sleep(remaining);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        // Reset the cycle clock for the next pass.
+        period.reset();
+    }
+
+
+}
