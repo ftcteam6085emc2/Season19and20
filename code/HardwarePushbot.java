859dfaf611983e40566173870b5e5e1abe2c903b
Todd V. Rovito <rovitotv@gmail.com>
2019-12-22 13:05:58 -0500

    Update to include linear slide

--- code/HardwarePushbot.java
+++ code/HardwarePushbot.java
@@ -29,7 +29,7 @@ public class HardwarePushbot
     public DcMotor  rightDrive  = null;
     public DcMotor  rightBack   = null;
     public DcMotor  leftBack    = null;
-    public DcMotor  leftArm     = null;
+    public DcMotor  hexMotor     = null;
     public Servo    leftClaw    = null;
     public Servo    rightClaw   = null;
 
@@ -52,30 +52,26 @@ public class HardwarePushbot
         hwMap = ahwMap;
 
         // Define and Initialize Motors
-        leftDrive  = hwMap.get(DcMotor.class, "left_drive");
-        rightDrive = hwMap.get(DcMotor.class, "right_drive");
-        rightBack  = hwMap.get(DcMotor.class, "right_back");
-        leftBack   = hwMap.get(DcMotor.class, "left_back");
-        leftArm    = hwMap.get(DcMotor.class, "left_arm");
+        leftDrive  = hwMap.get(DcMotor.class, "FrontLeft");
+        rightDrive = hwMap.get(DcMotor.class, "FrontRight");
+        rightBack  = hwMap.get(DcMotor.class, "RearRight");
+        leftBack   = hwMap.get(DcMotor.class, "RearLeft");
+        hexMotor    = hwMap.get(DcMotor.class, "hexMotor");
         leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
 
         // Set all motors to zero power
         leftDrive.setPower(0);
         rightDrive.setPower(0);
-        leftArm.setPower(0);
+        hexMotor.setPower(0);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
         leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        leftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        hexMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        // Define and initialize ALL installed servos.
-        leftClaw  = hwMap.get(Servo.class, "left_hand");
-        rightClaw = hwMap.get(Servo.class, "right_hand");
-        leftClaw.setPosition(MID_SERVO);
-        rightClaw.setPosition(MID_SERVO);
+        
     }
  }
 
